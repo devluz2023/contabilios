f231a6dc2d371f0956103360362daa8f1f3f76b6
binhojulix <fabio.jdluz@gmail.com>
2024-05-27 16:41:17 -0300

    Initial Commit

---
+++ contabilios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  contabilios
+//
+//  Created by fabio luz on 27/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ contabilios/contabiliosApp.swift
@@ -0,0 +1,17 @@
+//
+//  contabiliosApp.swift
+//  contabilios
+//
+//  Created by fabio luz on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct contabiliosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
